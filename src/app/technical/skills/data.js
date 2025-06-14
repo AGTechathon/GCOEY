b907b4fc50b384fcd89991bf1490956f4df383f8
nikhil kandhare <98297220+1900690105@users.noreply.github.com>
2025-06-15 04:08:15 +0530

    fourty commit

---
+++ src/app/technical/skills/data.js
@@ -0,0 +1,398 @@
+export const Day1 = {
+  date: "2025-01-10T06:00:00+05:30",
+  title: "13-Day Practice",
+  days: [
+    {
+      day: 1,
+      focus: "Active Listening",
+      chapters: [
+        {
+          chapter: 1,
+          title: "Maintain Eye Contact",
+          points: [
+            {
+              title: "Show Confidence and Engagement",
+              description:
+                "Maintain steady eye contact to demonstrate confidence and genuine interest in the conversation.",
+            },
+            {
+              title: "Balance Eye Contact and Breaks",
+              description:
+                "Hold eye contact for about 3 to 5 seconds at a time, then briefly glance away.",
+            },
+            {
+              title: "Adapt Eye Contact to Cultural Contexts",
+              description:
+                "Be mindful of cultural differences regarding direct eye contact.",
+            },
+            {
+              title: "Focus on the Entire Group",
+              description:
+                "Shift your eye contact between different people in group communication.",
+            },
+            {
+              title: "Use Eye Contact to Listen Actively",
+              description:
+                "Maintain eye contact while someone else is speaking to show attentiveness.",
+            },
+            {
+              title: "Use Eye Triangle Pattern",
+              description:
+                "Look at the left eye, right eye, and then mouth in a triangle pattern.",
+            },
+            {
+              title: "Follow the 70-30 Rule",
+              description:
+                "Spend about 70% of the time making eye contact and 30% looking away naturally.",
+            },
+          ],
+        },
+        {
+          chapter: 2,
+          title: "Give Full Attention",
+          points: [
+            {
+              title: "Eliminate Distractions",
+              description: "Turn off your phone and avoid multitasking.",
+            },
+            {
+              title: "Maintain Active Listening",
+              description:
+                "Use non-verbal cues like nodding and facial expressions.",
+            },
+            {
+              title: "Face the Speaker",
+              description:
+                "Position yourself directly facing the person you are communicating with.",
+            },
+            {
+              title: "Use Reflective Feedback",
+              description: "Paraphrase or summarize what the speaker has said.",
+            },
+            {
+              title: "Be Mindful and Stay in the Moment",
+              description:
+                "Concentrate on their words and emotions without preemptive judgments.",
+            },
+          ],
+        },
+        {
+          chapter: 3,
+          title: "Use Positive Body Language",
+          points: [
+            {
+              title: "Maintain an Open Posture",
+              description:
+                "Keep your body relaxed and open to signal engagement.",
+            },
+            {
+              title: "Smile Genuinely",
+              description: "A warm smile conveys friendliness.",
+            },
+            {
+              title: "Use Gestures to Enhance Communication",
+              description: "Complement your words with natural hand gestures.",
+            },
+            {
+              title: "Lean Slightly Forward",
+              description: "Shows interest while avoiding aggression.",
+            },
+            {
+              title: "Match Your Facial Expressions with Your Words",
+              description:
+                "Ensure your facial expressions align with the tone of your message. A cheerful expression while delivering positive news or a thoughtful expression during serious conversations creates alignment ",
+            },
+            {
+              title: "Use Open Hand Gestures",
+              description:
+                "Open hand gestures or placing your hands on the table can make you appear more welcoming and receptive.",
+            },
+            {
+              title: "Nod Occasionally",
+              description:
+                "Nodding your head while listening helps to communicate understanding and encourages the speaker to continue.",
+            },
+            {
+              title: "Looking Around",
+              description:
+                "While talking to someone, if that person looks here and there a lot or takes out his phone and opens and closes.",
+            },
+            {
+              title: "Knee Claps",
+              description:
+                "When you are sitting and talking with someone and that person is sitting with both his hands on his knees, leaning slightly forward, then this signal indicates that the person wants to get out of there as soon as possible",
+            },
+            {
+              title: "Feet Direction",
+              description:
+                "The direction a person's feet are pointing can indicate their level of interest in a conversation, with feet pointed towards you suggesting engagement and feet pointed away indicating disinterest.",
+            },
+            {
+              title: "Throat Clearing",
+              description:
+                "If a person clears their throat before or after answering your question, it may suggest they are feeling nervous or uncomfortable, potentially signaling they are trying to hide something or are hesitant to tell the truth.",
+            },
+            {
+              title: "Putting Hands In Pocket",
+              description:
+                "Putting one's hands in their pockets can indicate feelings of discomfort, defensiveness, or a desire to distance oneself from the conversation.",
+            },
+            {
+              title: "Eye Pupil.",
+              description:
+                "Dilated pupils can indicate interest, arousal or excitement, while constricted pupils may suggest discomfort, fear or lack of interest.",
+            },
+          ],
+        },
+        {
+          chapter: 4,
+          title: "Avoid Interrupting",
+          points: [
+            {
+              title: "Listen Actively Without Interjecting",
+              description:
+                "Focus entirely on the speaker’s words without thinking about your response. Resist the urge to complete sentences or jump in with your opinion before they finish speaking.",
+            },
+            {
+              title: "Wait for a Natural Pause",
+              description:
+                "Allow the speaker to pause or finish their thought before contributing. This shows patience and attentiveness, creating a more balanced conversation.",
+            },
+            {
+              title: "Use Non-Verbal Cues to Show Engagement",
+              description:
+                "Instead of interrupting verbally, use nods or facial expressions to signal that you are following along and understanding what is being said.",
+            },
+            {
+              title: "Acknowledge Your Turn to Speak",
+              description:
+                "Politely indicate when you want to add something by saying, “I’d like to add to that point”or “When you’re done, I have a thought to share.”This respects the flow of the conversation.",
+            },
+            {
+              title: "Practice Empathy and Respect",
+              description:
+                "Recognize the importance of letting others express themselves fully. Value their input by giving them uninterrupted space to communicate, which fosters trust and mutual respect.",
+            },
+            {
+              title: "Control Your Impulses",
+              description:
+                "If you feel the urge to interrupt, remind yourself thatit’s important to let the speaker finish. Take a breath or mentally note what you want to say, but wait until they're done.",
+            },
+            {
+              title: "Clarify, Don't Correct",
+              description: `If you feel the need to correct or add something, do so in a clarifying way once they’ve finished. Use phrases like "I understand, but can you explain more about..." instead of jumping in.`,
+            },
+          ],
+        },
+        {
+          chapter: 5,
+          title: "Paraphrase and Summarize",
+          points: [
+            {
+              title: "Paraphrase",
+              description:
+                "Paraphrasing involves rewording a piece of text to express the same idea in a different way. The goal is to maintain the original meaning while changing the sentence structure and wording. It is usually of a similar length to the original text.",
+            },
+            {
+              title: "Summarize",
+              description:
+                "Summarizing involves condensing the original text to its main points, leaving out details. The goal is to create a shorter version that retains only the essential information.",
+            },
+            {
+              title: "Identify the Main Idea",
+              description:
+                "Focus on the core message or key points of what the speaker is saying to ensure you capture the essence of the conversation.",
+            },
+            {
+              title: "Use Your Own Words",
+              description:
+                "When paraphrasing, restate the speaker’s ideas in your own words without altering the original meaning. For example, “You’re saying that..",
+            },
+            {
+              title: "Keep It Concise for Summarizing",
+              description: `Highlight only the most important points when summarizing, leaving out unnecessary details. Example: "To summarize, you need more support in organizing the even."`,
+            },
+            {
+              title: "Confirm Understanding",
+              description:
+                "Ask for confirmation to ensure your paraphrase or summary is accurate. Use phrases like, “Is that correct?” or “Did I understand that right?.",
+            },
+            {
+              title: "Avoid Adding Opinions or Interpretations",
+              description:
+                "Stick to the speaker's message without inserting your biases. The goal is to clarify and condense the message, not to provide your viewpoint.",
+            },
+          ],
+        },
+        {
+          chapter: 6,
+          title: "Ask Open-Ended Questions",
+          points: [
+            {
+              title: "Beginwith ‘What,’ ‘How,’ or ‘Why’",
+              description:
+                "Frame questions starting with these words to encourage detailed responses. Example: “What do you think about this idea?”rather than “Do you like this idea?”.",
+            },
+            {
+              title: "Encourage Elaboration",
+              description:
+                "Ask questions that invite more than a simple yes or no, prompting deeper thinking and expanded answers. Example: “How did you approach solving that problem?”",
+            },
+            {
+              title: "Focus on Exploration and Insights",
+              description:
+                "Use open-ended questions to explore feelings, opinions, and perspectives. Example: “Why do you feel this solution is effective?”.",
+            },
+            {
+              title: "Avoid Leading Questions",
+              description:
+                "Avoid questions that imply a specific answer or judgment. Instead of “Don’t you think this is the best option?”ask, “What options do you think are most effective?”.",
+            },
+            {
+              title: "Follow Up with Clarifying Questions",
+              description:
+                "When answers are vague, ask follow-up questions to get more clarity. Example: “Can you explain more about what you mean?”.",
+            },
+            {
+              title: "Avoid Yes/No Questions",
+              description: `Open-ended questions allow the person to express their thoughts, feelings, and experiences more fully. Avoid questions that can be answered with a simple "yes" or "no." For example:Instead of: "Did you like the event?",Ask: "What did you like most about the event?`,
+            },
+            {
+              title: "Be Curious and Non-Judgmental",
+              description: `Maintain a genuine curiosity to understand the speaker's perspective without judgment. Ask questions that reflect this curiosity: What was the thought process behind that decision?""How did you come to that conclusion?"`,
+            },
+            {
+              title: "Use the Speaker's Own Words in Your Questions",
+              description:
+                "Reflect their language back to them to demonstrate that you’re paying attention:You mentioned feeling overwhelmed—what was it about the situation that caused that?",
+            },
+          ],
+        },
+        {
+          chapter: 7,
+          title: "Give Verbal Acknowledgments",
+          points: [
+            {
+              title: "Use Affirmative Words and Phrases",
+              description:
+                "Show you are listening by saying, “I understand,”“That makes sense,”or “I see what you mean.”This encourages the speaker to continue sharing.",
+            },
+            {
+              title: "Repeat Key Points",
+              description:
+                "Reiterate important points or words from the speaker to demonstrate active engagement. Example: “So, you’re saying the new strategy improved productivity.”.",
+            },
+            {
+              title: "Express Agreement or Empathy",
+              description:
+                "Acknowledge emotions or agree where appropriate with statements like, “That sounds exciting!”or “I can imagine how challenging that was.",
+            },
+            {
+              title: "Use Encouraging Phrases",
+              description:
+                "Encourage the speaker to elaborate by saying, “Tell me more about that,”or “Could you expand on that idea?”.",
+            },
+            {
+              title: "Thank the Speaker",
+              description:
+                "Show appreciation for their input or feedback. Example: “Thank youfor sharing your thoughts; it’s really helpful.”.",
+            },
+          ],
+        },
+        {
+          chapter: 8,
+          title: "Avoid Judgments or Immediate Conclusions",
+          points: [
+            {
+              title: "Listen Without Bias",
+              description:
+                "Focus on understanding the speaker’s message without allowing preconceived notions or assumptions to influence your interpretation. Stay curious and open-minded.",
+            },
+            {
+              title: "Pause Before Responding",
+              description:
+                "Take a moment to process what you’ve heard before forming a conclusion. Reflecting first helps avoid reactive or judgmental statements",
+            },
+            {
+              title: "Ask Clarifying Questions",
+              description:
+                "Seek more information to fully understandthe context. Example: “Can you explain why you feel that way?”instead of jumping to conclusions.",
+            },
+            {
+              title: "Acknowledge Multiple Perspectives",
+              description:
+                "Recognize there may be different viewpoints or solutions. Use phrases like, “I can see how that makes sense from your perspective.”.",
+            },
+            {
+              title: "Respond with Empathy and Neutral Language",
+              description:
+                "Avoid emotionally charged or judgmental words. Instead, use neutral, supportive language like, “That’s an interesting point,”rather than labeling ideas as right or wrong.",
+            },
+          ],
+        },
+        {
+          chapter: 9,
+          title: "Respond Thoughtfully",
+          points: [
+            {
+              title: "Take Time to Process Information",
+              description:
+                "Pause briefly to think before responding, ensuring your reply is clear, relevant, and considerate. Avoid rushing into answers without full understanding.",
+            },
+            {
+              title: "Acknowledge the Speaker’s Perspective",
+              description:
+                "Show empathy and validation with phrases like, “I understand your point,”or “That must have been challenging.”This demonstrates respect for their thoughts and feelings",
+            },
+            {
+              title: "Provide Constructive and Positive Feedback",
+              description:
+                "Offer solutions or insights thoughtfully. Use language that is helpful and forward-thinking, such as, “Perhaps we could try...”instead of focusing on negatives.",
+            },
+            {
+              title: "Ask for More Details if Needed",
+              description:
+                "If something isn’t clear, ask questions to clarify. Example: “Could you explain more about what you mean by...?”to ensure your responsealigns with their intent.",
+            },
+            {
+              title: "Stay Focused and Relevant",
+              description:
+                "Keep your response aligned with the topic of conversation. Avoid tangents or unrelated points to show you value the subject being discussed.",
+            },
+          ],
+        },
+        {
+          chapter: 10,
+          title: "Provide Feedback When Appropriate",
+          points: [
+            {
+              title: "Be Specific and Clear",
+              description:
+                "Focus on particular actions or behaviors rather than generalizations. Example: “Your presentation was well-structured, especially the clear examples,”instead of “Good job.”.",
+            },
+            {
+              title: "Balance Positive and Constructive Feedback",
+              description:
+                "Recognize strengths before discussing areas for improvement. Example: “You communicated your ideas effectively. One area to enhance would be using more visuals to keep the audience engaged.”",
+            },
+            {
+              title: "Be Timely",
+              description:
+                "Provide feedback soon after the event or action, while it’s fresh and relevant, to ensure it has the greatest impact.",
+            },
+            {
+              title: "Use a Respectful and Supportive Tone",
+              description:
+                "Deliver feedback in a way that motivates rather than demoralizes. Example: “I noticed you’ve been putting in great effort, but I think adjusting this approach could yield even better results.”.",
+            },
+            {
+              title: "Make It Actionable",
+              description:
+                "Suggest  practical  ways  to  improve,  providing  specific  guidance.  Example: “Next time, consider organizing your points into shorter sections to make them easier to follow.”.",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
